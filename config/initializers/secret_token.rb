cc70f6954f71ca5d7e18e050581e33cff7c7b891
Kirillov Alexander <saratovsource@gmail.com>
2013-06-13 23:41:25 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tagit::Application.config.secret_key_base = '44fd11d87df5ec243c270f03c70c886daf234994b36d9bf02d4004e41c7288150ce8b7d7bc6864436c9b122cf6bf7527a80d7841a13961e038dc3d914761849c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tagit::Application.config.session_store :cookie_store, key: '_tagit_session'
